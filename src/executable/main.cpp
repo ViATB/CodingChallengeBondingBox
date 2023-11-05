33b24fd3744fa68daeeff0d2df33f33597482027
TB <donaldspam@gmx.de>
2023-11-05 14:14:34 +0100

    added opencv as submodule

--- src/executable/main.cpp
+++ src/executable/main.cpp
@@ -1,46 +1,75 @@
 #include <iostream> // for standard I/O
 #include <string>   // for strings
-#include <opencv2/core.hpp>     // Basic OpenCV structures (cv::Mat, Scalar)
-#include <opencv2/videoio.hpp>
+#include <opencv2/opencv.hpp>
+//#include <opencv2/core.hpp>     // Basic OpenCV structures (cv::Mat, Scalar)
+//#include <opencv2/videoio.hpp>
 #include <opencv2/highgui.hpp>  // OpenCV window I/O
-
-const char* XPATH = "my/path";
+#include <opencv2/tracking.hpp>
 
 using namespace std;
 
 int main(int argc, char* argv[])
 {
-    if (argc != 3)
+    if (argc != 2)
     {
         std::cout << "Please add a video file path.";
         return -1;
     }
 
-    const string sourceReference = argv[1];
-    const string sourceCompareWith = argv[2];
+    const string source = argv[1];
 
-    int frameNum = -1;          // Frame counter
-    cv::VideoCapture captRefrnc(sourceReference);
-    cv::VideoCapture captUndTst(sourceCompareWith);
+    // create video capturing object
+    //cv::VideoCapture captVideo(source);
+    cv::VideoCapture captVideo(source);
 
-    if (! captRefrnc.isOpened())
+    if (! captVideo.isOpened())
     {
-        cout << "Could not open reference " << sourceReference << endl;
+        cout << "Could not open video file: " << source << endl;
         return -1;
     }
 
-    cv::Size refS = cv::Size(
-        static_cast<int>(captRefrnc.get(cv::CAP_PROP_FRAME_WIDTH)),
-        static_cast<int>(captRefrnc.get(cv::CAP_PROP_FRAME_HEIGHT))
-    );
-    cv::Size uTSi = cv::Size(
-        static_cast<int>(captUndTst.get(cv::CAP_PROP_FRAME_WIDTH)),
-        static_cast<int>(captUndTst.get(cv::CAP_PROP_FRAME_HEIGHT))
-    );
+    // Get video resolution
+    int frameWidth = static_cast<int>(captVideo.get(cv::CAP_PROP_FRAME_WIDTH));
+    int frameHeigth = static_cast<int>(captVideo.get(cv::CAP_PROP_FRAME_HEIGHT));
+
+    cv::Size size = cv::Size(frameWidth, frameHeigth);
+
+    cout << "Reference frame resolution: Width=" << size.width << "  Height=" << size.height
+        << " of nr#: " << captVideo.get(cv::CAP_PROP_FRAME_COUNT) << endl;
 
 
-    cout << "Reference frame resolution: Width=" << refS.width << "  Height=" << refS.height
-        << " of nr#: " << captRefrnc.get(cv::CAP_PROP_FRAME_COUNT) << endl;
+    // Create video writer object
+    cv::VideoWriter output("output.avi", cv::VideoWriter::fourcc('M', 'J', 'P', 'G'), 30, size);
+
+    cv::Mat frame;
+    //cv::Ptr<cv::Tracker> tracker = cv::TrackerNano::create();
+    cv::Ptr<cv::Tracker> tracker = cv::TrackerKCF::create();
+    cv::Rect trackingBox = cv::selectROI(frame, false);
+    tracker->init(frame, trackingBox);
+
+    while (captVideo.read(frame))
+    {
+        if (tracker->update(frame, trackingBox))
+        {
+            cv::rectangle(frame, trackingBox, cv::Scalar(255, 0, 0), 2, 8);
+        }
+
+        // Display the frame
+        cv::imshow("Video feed", frame);
+
+        // Write video frame to output
+        output.write(frame);
+
+        // For breaking the loop
+        if (cv::waitKey(25) >= 0) break;
+    }
+        
+    // Release video capture and writer
+    output.release();
+    captVideo.release();
+
+    // Destroy all windows
+    cv::destroyAllWindows();
 
     return 0;
 }
